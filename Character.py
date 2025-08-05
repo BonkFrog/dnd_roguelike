9b358a9b186e0f8cb4a500c11564a9120443daa7
Agave <222908487+BonkFrog@users.noreply.github.com>
2025-08-05 18:56:03 +0900

    made the print into the __str__

--- Character.py
+++ Character.py
@@ -26,8 +26,37 @@ class chara:
     
     # defines how a class gets represented for the user.
     def __str__(self):
+        # Get the max count for the property names string len and add 2 spaces 
+        str_count = [len(item[0]) for item in self]
+        max_spaces = max(str_count) + 2 
+        
+        character_sheet = []
+        
+        for item in self:
+            
+            # Same thing for ability score cause that is nested dictionary, we want to print it nicely.
+            if item[0] == "ability_score":
+                # print out the ability score part so it looks nicer.
+                character_sheet.append(f"{item[0]}".ljust(max_spaces))
+
+                ability_score = item[1]
+                score_count = [len(score) for score in ability_score]
+                max_score_spaces = max(score_count) + 7
+
+                for score in ability_score:
+                    # this is how to seperate the name and values of a dictionary.
+                    score_name, score_value = list(score.items())[0]
+                    character_sheet.append(f"   {score_name}".ljust(max_score_spaces) + "|  " + f"{score_value}")
+                continue
+
+            # Do later when I implement loot.
+            if item[0] == "inventory":
+                #print(True)
+                pass
+            character_sheet.append(f"{item[0]}".ljust(max_spaces) + "|  " + f"{item[1]}")
+        return "\n".join([line for line in character_sheet])
         # vars(), this returns properties and values in dictionary format for the variable specified.
-        return "\n".join([f"{attr}: {value}" for attr, value in vars(self).items()])
+        #return "\n".join([f"{attr}: {value}" for attr, value in vars(self).items()])
 
     # This is how to may my object iterable
     def __iter__(self):
@@ -36,26 +65,4 @@ class chara:
     
 Test = chara(player = "Derick", chara_name = "Edwin", chara_class = "Warrior")
 
-str_count = [len(item[0]) for item in Test]
-max_spaces = max(str_count) + 2
-
-for item in Test:
-    if item[0] == "ability_score":
-        ability_score = item[1]
-        score_count = [len(score) for score in ability_score]
-        max_score_spaces = max(score_count) + 7
-
-        print(f"{item[0]}".ljust(max_spaces))
-
-        for score in ability_score:
-            # this is how to seperate the name and values
-            score_name, score_value = list(score.items())[0]
-            print(f"   {score_name}".ljust(max_score_spaces) + "|  " + f"{score_value}")
-
-        continue
-
-    # Do later when I implement loot.
-    if item[0] == "inventory":
-        #print(True)
-        pass
-    print(f"{item[0]}".ljust(max_spaces) + "|  " + f"{item[1]}")
+print(Test)
