bc41ab7872265372ff8f411b2eabcfac9916b1bf
Nickolas barrett <222908487+BonkFrog@users.noreply.github.com>
2025-08-08 21:33:43 +0900

    started adding loot generation logic, want to flesh out the accessories generation and consumables.

--- loot_generator.py
+++ loot_generator.py
@@ -2,17 +2,30 @@ import os
 import csv
 import random
 
-def read_loot_csv(reward_db_location):
-    with open(reward_db_location, mode="r",newline="") as reward_csv:
+def read_csv(csv_file):
+    with open(csv_file, mode="r",newline="",encoding='utf-8') as reward_csv:
         Reader = csv.DictReader(reward_csv)
         rows = list(Reader)
     return rows
+
+# Parameters set for loot generation.
 if os.name == 'nt':
     reward_db_location = os.path.dirname(__file__) + "\\" + "Reward_DB.csv"
+    weapon_db = os.path.dirname(__file__) + "\\" + "Weapons.csv"
+    spell_db = os.path.dirname(__file__) + "\\" + "Spells.csv"
+    armors_db = os.path.dirname(__file__) + "\\" + "Armors.csv"
+    enchantments_db = os.path.dirname(__file__) + "\\" + "Enchantments.csv"
+
 if os.name == 'posix':
     reward_db_location = os.path.dirname(__file__) + "/" + "Reward_DB.csv"
+    weapon_db = os.path.dirname(__file__) + "/" + "Weapons.csv"
+    spell_db = os.path.dirname(__file__) + "/" + "Spells.csv"
+    armors_db = os.path.dirname(__file__) + "/" + "Armors.csv"
+    enchantments_db = os.path.dirname(__file__) + "/" + "Enchantments.csv"
 
-reward_db = read_loot_csv(reward_db_location)
+reward_db = read_csv(reward_db_location)
+
+# we can slightly change the weights probably by adding more weight to one but that can be a later mechanic.
 
 ItemCategory_Weights = [{"Category": "Weapon", "Weight": 0.4},
     {"Category": "Armor", "Weight": 0.4},
@@ -23,9 +36,41 @@ ItemCategory_Weights = [{"Category": "Weapon", "Weight": 0.4},
 itemType = [lootType['Category'] for lootType in ItemCategory_Weights]
 item_Weights = [lootType['Weight'] for lootType in ItemCategory_Weights]
 
+generated_loot_types = random.choices(itemType, weights=item_Weights, k=3)
+for loot_type in generated_loot_types:
+    if loot_type == "Weapon":
+        weapons = read_csv(weapon_db)
+        random.choice(weapons)
+    if loot_type == "Armor":
+        armors = read_csv(armors_db)
+        armor_weights = [float(armor['Weight']) for armor in armors]
+        selected_armor = random.choices(armors, weights=armor_weights, k=1)[0]
+        #print(selected_armor)
+    if loot_type == "Blessing":
+        blessings = [item for item in reward_db if item['Category'] == "Blessing"]
+        blessing_weights = [float(item['Weight']) for item in blessings]
+        blessing = random.choices(blessings, weights=blessing_weights, k=1)[0]
+        print(blessing)
+
+    if loot_type == "Consumable":
+        # Want to randomize between spell scrolls and consumables.
+        consumable_type = [{"Category": "Consumable", "Weight": 0.7},{"Category": "Spells", "Weight": 0.6}]
+        consumable_weight = [float(item_type["Weight"]) for item_type in consumable_type]
+        consumable_type = [item_type["Category"] for item_type in consumable_type]
+        item_type = random.choices(consumable_type, weights=consumable_weight)
+    if loot_type == "Accessory":
+        pass
+
+#print(generated_loot_types)
 # just checking item spread.
 #for i in range(0, 100):
 #    print(random.choices(itemType, weights=item_Weights, k=3))
+
+
+
+#for loot_type in generated_loot_types:
+
+
 
 #for items in enumerate(reward_db):
 #    print(items[1]['Weight'])
